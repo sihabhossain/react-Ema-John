2998929e0d605414cc72f3aee63547963a19302d
Sihab Hossain <sihabhossain380@gmail.com>
2023-03-26 23:58:55 +0600

    data loaded from local file

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import "./App.css";
 import Header from "./components/Header/Header";
+import Shop from "./components/Shop/Shop";
 
 function App() {
-  return <Header></Header>;
+  return (
+    <div>
+      <Header></Header>
+      <Shop></Shop>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Shop/Shop.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+import "./Shop.css";
+
+const Shop = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch("products.json")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+  return (
+    <div className="shop-container">
+      <div className="products-container">
+        <h1>hey this is products:{products.length}</h1>
+      </div>
+      <div className="cart-container">
+        <h2>hello i am cart</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Shop;
