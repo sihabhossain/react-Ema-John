b78349d9b766003acb240bb2b76f569c50f422c7
Sihab Hossain <sihabhossain380@gmail.com>
2023-03-27 17:37:01 +0600

    shoes added to website

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -3,11 +3,18 @@ import "./Product.css";
 
 const Product = (props) => {
   //   console.log(props.product);
-  const { name, img, seller, quantity, price } = props.product;
+  const { name, img, seller, ratings, price } = props.product;
   return (
     <div className="product-container">
-      <img src={img} alt="" />
-      <h3>{name}</h3>
+      <div>
+        <img src={img} alt="" />
+        <h3 className="product-name">{name}</h3>
+        <p>Price: ${price}</p>
+
+        <p>Seller:{seller}</p>
+        <p>Rating: {ratings} stars</p>
+      </div>
+      <button className="btn-cart">Add To Cart</button>
     </div>
   );
 };
