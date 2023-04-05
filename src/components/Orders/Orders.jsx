a26cab452867ce20f1232908e9b15a7047484b29
Sihab Hossain <sihabhossain380@gmail.com>
2023-04-05 17:32:43 +0600

    styles added to the orders component

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import Cart from "../Cart/Cart";
 import { useLoaderData } from "react-router-dom";
+import ReviewItem from "../ReviewItem/ReviewItem";
+import "./Orders.css";
 
 const Orders = () => {
   const cart = useLoaderData();
   console.log(cart);
   return (
     <div className="shop-container">
-      <div className="products-container">
-        <h1>order page: {cart.length}</h1>
+      <div className="review-container">
+        {cart.map((product) => (
+          <ReviewItem key={product.id} product={product}></ReviewItem>
+        ))}
       </div>
       <div className="cart-container">
         <Cart cart={cart}></Cart>
---
+++ src/components/ReviewItem/ReviewItem.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./ReviewItem.css";
+
+const ReviewItem = ({ product }) => {
+  return (
+    <div className="review-item">
+      <h2>Review Item</h2>
+    </div>
+  );
+};
+
+export default ReviewItem;
