f513e4d5d2164a7c1d587b9bde9dd4ef923f0b8b
Sihab Hossain <sihabhossain380@gmail.com>
2023-04-05 18:04:26 +0600

    more styles and delete button added

--- src/components/ReviewItem/ReviewItem.jsx
+++ src/components/ReviewItem/ReviewItem.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import "./ReviewItem.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
 
 const ReviewItem = ({ product }) => {
+  const { id, img, price, name, quantity } = product;
+
   return (
     <div className="review-item">
-      <h2>Review Item</h2>
+      <img src={img} alt="" />
+      <div className="review-details">
+        <p className="product-title">Name: {name}</p>
+        <p>
+          Price: <span className="orange-text">${price}</span>
+        </p>
+        <p>
+          Order Quantity: <span className="orange-text">{quantity}</span>
+        </p>
+      </div>
+      <button className="btn-delete">
+        <FontAwesomeIcon icon={faTrashCan} />
+      </button>
     </div>
   );
 };
