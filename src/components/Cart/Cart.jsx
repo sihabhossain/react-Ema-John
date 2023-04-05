618022a9e875e59d54c6de84109bd2ee69dded48
Sihab Hossain <sihabhossain380@gmail.com>
2023-04-05 19:48:07 +0600

    clear cart button added

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,39 +1,45 @@
-import React from 'react';
-import './Cart.css';
+import React from "react";
+import "./Cart.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
 
-const Cart = ({ cart }) => {
-    // const cart = props.cart; // option 1
-    // const {cart} = props; // option 2
+const Cart = ({ cart, HandleClearCart }) => {
+  // const cart = props.cart; // option 1
+  // const {cart} = props; // option 2
 
-    // console.log(cart);
+  // console.log(cart);
 
-    let totalPrice = 0;
-    let totalShipping = 0;
-    let quantity = 0;
-    for (const product of cart) {
-        // if(product.quantity === 0){
-        //     product.quantity = 1;
-        // }
-        // product.quantity = product.quantity || 1;
+  let totalPrice = 0;
+  let totalShipping = 0;
+  let quantity = 0;
+  for (const product of cart) {
+    // if(product.quantity === 0){
+    //     product.quantity = 1;
+    // }
+    // product.quantity = product.quantity || 1;
 
-        totalPrice = totalPrice + product.price * product.quantity;
-        totalShipping = totalShipping + product.shipping;
-        quantity = quantity + product.quantity;
-    }
-    const tax = totalPrice * 7 / 100;
+    totalPrice = totalPrice + product.price * product.quantity;
+    totalShipping = totalShipping + product.shipping;
+    quantity = quantity + product.quantity;
+  }
+  const tax = (totalPrice * 7) / 100;
 
-    const grandTotal = totalPrice + totalShipping + tax;
+  const grandTotal = totalPrice + totalShipping + tax;
 
-    return (
-        <div className='cart'>
-            <h4>Order Summary</h4>
-            <p>Selected Items: {quantity}</p>
-            <p>Total Price: ${totalPrice}</p>
-            <p>Shipping: ${totalShipping}</p>
-            <p>Tax: ${tax.toFixed(2)}</p>
-            <h6>Grand Total: ${grandTotal.toFixed(2)} </h6>
-        </div>
-    );
+  return (
+    <div className="cart">
+      <h4>Order Summary</h4>
+      <p>Selected Items: {quantity}</p>
+      <p>Total Price: ${totalPrice}</p>
+      <p>Shipping: ${totalShipping}</p>
+      <p>Tax: ${tax.toFixed(2)}</p>
+      <h6>Grand Total: ${grandTotal.toFixed(2)} </h6>
+      <button onClick={HandleClearCart} className="btn-clear-cart">
+        <span>Clear Cart</span>
+        <FontAwesomeIcon icon={faTrashCan} />
+      </button>
+    </div>
+  );
 };
 
 export default Cart;
